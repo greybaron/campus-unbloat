494917890c05031f8a62b6d9424cbbf63eae7c56
Florian Schmidt <flo.schmidt10@gmail.com>
2024-07-16 22:01:36 +0200

    improve infotile, add fullscreen "button" to tiles, move stuff around

--- src/lib/TilesAndModals/BasicInfoTile.svelte
+++ src/lib/TilesAndModals/BasicInfoTile.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import DashboardTile from '$lib/DashboardTile.svelte';
+	import { ProgressBar } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
 	export let basic_data: {
 		first_name: string;
@@ -28,22 +29,27 @@
 </script>
 
 <DashboardTile
-	title="Daten zur Person"
+	title="Deine Infos"
 	clickable={false}
 	ready={Boolean(basic_data && fachsemester && ects)}
 >
 	<svelte:fragment slot="body">
-		{#if basic_data}
-			<div class="flex flex-col h-full justify-center space-y-2">
-				<p>{basic_data.user} - {basic_data.last_name}, {basic_data.first_name}</p>
-				<p>{basic_data.seminar_group} - {basic_data.seminar_name}</p>
-			</div>
-		{/if}
-		{#if fachsemester}
-			<p>Fachsemester: {fachsemester}</p>
-		{/if}
-		{#if ects}
-			<p>ECTS: {ects} / 180</p>
-		{/if}
+		<div class="h-full flex flex-col justify-around items-center">
+			{#if basic_data}
+				<p>{basic_data.user} - {basic_data.seminar_group}</p>
+				<p>{basic_data.seminar_name}</p>
+			{/if}
+			<div class="h-2" />
+			{#if fachsemester && ects}
+				<div class="flex w-full justify-between p-1">
+					<p>Fachsemester: {fachsemester}</p>
+					<p>ECTS: {ects} / 180</p>
+				</div>
+				<div class="flex w-full justify-between space-x-2">
+					<ProgressBar value={parseInt(fachsemester)} max={6} />
+					<ProgressBar value={parseInt(ects)} max={180} />
+				</div>
+			{/if}
+		</div>
 	</svelte:fragment>
 </DashboardTile>
