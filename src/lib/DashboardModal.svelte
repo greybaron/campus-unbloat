33d6f93a17d6f339887ac0bfed1531ea78b0f1f4
Florian Schmidt <flo.schmidt10@gmail.com>
2024-07-23 20:10:25 +0200

    add dynamicwidth property to modal to allow for larger width

--- src/lib/DashboardModal.svelte
+++ src/lib/DashboardModal.svelte
@@ -3,10 +3,13 @@
 
 	export let title: string;
 	export let parent: SvelteComponent;
+	export let dynamicwidth: boolean = false;
 </script>
 
 <div
-	class="modal block overflow-y-auto bg-surface-100-800-token w-modal h-auto p-4 space-y-4 rounded-container-token shadow-xl"
+	class="{dynamicwidth
+		? ''
+		: 'w-modal'} modal block overflow-y-auto bg-surface-100-800-token h-auto p-4 space-y-4 rounded-container-token shadow-xl"
 	role="dialog"
 >
 	<div class="flex w-full items-center">
@@ -18,7 +21,7 @@
 			class="size-8 fa-solid fa-xmark scale-150"
 		/>
 	</div>
-	<slot name="body" />
+	<slot />
 	<footer class="modal-footer flex justify-end space-x-2">
 		<slot name="optionalbuttons" />
 	</footer>
