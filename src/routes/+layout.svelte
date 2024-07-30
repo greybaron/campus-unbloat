c9117d05a52066027890baf89b858b48be3567eb
Florian Schmidt <flo.schmidt10@gmail.com>
2024-07-30 23:52:30 +0200

    move reminder fetching to dashboard; add count badge and loading animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,7 @@
 	import '@fortawesome/fontawesome-free/css/all.min.css';
 	import '../app.postcss';
 
-	import {
-		AppShell,
-		AppBar,
-		LightSwitch,
-		getToastStore,
-		ProgressRadial
-	} from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, LightSwitch, getDrawerStore } from '@skeletonlabs/skeleton';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
@@ -18,64 +12,8 @@
 	import { initializeStores, Modal, Toast, Drawer } from '@skeletonlabs/skeleton';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	import {
-		getToastSettings,
-		ToastPayloadClass,
-		type CdReminders,
-		type ToastPayload
-	} from '$lib/types';
-	import { onMount } from 'svelte';
 
 	initializeStores();
-
-	const toastStore = getToastStore();
-	let reminders: CdReminders;
-	let presentNotificationCategories: number = 0;
-
-	onMount(async () => {
-		console.log('Fetching reminders...');
-		const res = await fetch('/api/reminders');
-
-		if (!res.ok) {
-			let error = await res.text();
-			let payload: ToastPayload = {
-				text: error,
-				class: ToastPayloadClass.error
-			};
-
-			const toastSettings = getToastSettings(payload);
-			toastStore.trigger(toastSettings);
-		} else {
-			reminders = await res.json();
-			presentNotificationCategories = getPresentNotificationCategories(reminders);
-			console.log(presentNotificationCategories);
-		}
-	});
-
-	function getPresentNotificationCategories(reminders: CdReminders): number {
-		let presentNotificationCategories = 0;
-
-		if (reminders.latest.length > 0) {
-			presentNotificationCategories++;
-		}
-
-		if (reminders.upcoming.length > 0) {
-			presentNotificationCategories++;
-		}
-
-		if (reminders.exams > 0) {
-			presentNotificationCategories++;
-		}
-
-		if (reminders.electives > 0 && reminders.semester <= 6) {
-			presentNotificationCategories++;
-		}
-
-		return presentNotificationCategories;
-	}
-
-	// let res =
-	// 	'{"electives":0,"exams":1,"latest":[{"acad_session":"Sommerperiode","acad_year":"Akad. Jahr 2023/2024","agrdate":"20240717","agrtype":"Teilleistungsbeurteilung","awobject":"P Integrierte Informationssysteme (C)","awobject_short":"5CS-ERPS-00","awotype":"Studienmodul","awstatus":"Erfolgreich abgeschlossen","bookdate":"20240718","bookreason":"","cpgraded":"  0.00000","cpunit":"ECTS-Credits","gradesymbol":"1,3"}],"semester":7,"upcoming":[{"beguz":"090000","comment":"Prüfung (SEP)","enduz":"110000","evdat":"20240903","instructor":"diverse","location":"","objid":"00000000","room":"104 Seminarraum","sinstructor":"","sm_short":"5CS-MEDIT-00","sm_stext":"P Medizinisches Informationsmanagem. (K)","sroom":"5SR 104"},{"beguz":"080000","comment":"Prüfung (SEP)","enduz":"000000","evdat":"20240910","instructor":"Prof. Dr. Siegmund","location":"","objid":"00000000","room":"104 Seminarraum","sinstructor":"","sm_short":"5CS-V3DA-02","sm_stext":"P2 Videotech., 3D-Modell. u. Animat. (P)","sroom":"5SR 104"},{"beguz":"080000","comment":"Prüfung (SEP)","enduz":"000000","evdat":"20240912","instructor":"diverse","location":"","objid":"00000000","room":"104 Seminarraum","sinstructor":"","sm_short":"5CS-STDS-02","sm_stext":"P2 Servers. Techn. u. vert. Systeme (P)","sroom":"5SR 104"},{"beguz":"080000","comment":"Prüfung (SEP)","enduz":"000000","evdat":"20240912","instructor":"diverse","location":"","objid":"00000000","room":"104 Seminarraum","sinstructor":"","sm_short":"5CS-STDS-01","sm_stext":"P1 Servers. Techn. u. vert. Systeme (SE)","sroom":"5SR 104"}]}';
 
 	function formatDate(abapdate: string) {
 		const year = abapdate.slice(0, 4);
@@ -84,34 +22,30 @@
 
 		return `${day}.${month}.${year}`;
 	}
+
+	const drawerStore = getDrawerStore();
 </script>
 
 <Toast buttonDismiss="btn-icon btn-icon-sm variant-filled transition-none" />
 <Drawer
+	on:drawer={() => drawerStore.close()}
 	position="right"
 	width="min-w-0 ml-12"
-	bgDrawer="variant-glass-primary bg-white/30"
+	bgDrawer="variant-glass-primary bg-red-200/30"
 	bgBackdrop=""
 	regionDrawer="p-8"
 >
 	<div class="flex flex-col justify-center h-full space-y-4">
-		{#if !reminders}
-			<ProgressRadial
-				width="w-20 m-9"
-				stroke={80}
-				value={undefined}
-				strokeLinecap="round"
-				track="stroke-surface-500/30 dark:stroke-surface-300/30"
-			/>
-		{:else}
-			{#if reminders.latest.length > 0}
+		{#if $drawerStore.meta.reminders}
+			{#if $drawerStore.meta.reminders.latest.length > 0}
 				<div>
 					<h3 class="h3 font-bold">Letze Prüfungsergebnisse</h3>
 
 					<ul class="list-disc list-inside text-lg">
-						{#each reminders.latest as item}
+						{#each $drawerStore.meta.reminders.latest as item}
 							<li>
-								{formatDate(item.agrdate)}: {item.awobject} ({item.awobject_short}) -
+								<strong>{formatDate(item.agrdate)}</strong>: {item.awobject} ({item.awobject_short})
+								-
 								<strong>{item.gradesymbol}</strong>
 							</li>
 						{/each}
@@ -119,29 +53,30 @@
 				</div>
 			{/if}
 
-			{#if reminders.upcoming.length > 0}
+			{#if $drawerStore.meta.reminders.upcoming.length > 0}
 				<div>
 					<h3 class="h3 font-bold">Anstehende Prüfungen</h3>
 					<ul class="list-disc list-inside text-lg">
-						{#each reminders.upcoming as item}
+						{#each $drawerStore.meta.reminders.upcoming as item}
 							<li>
-								{formatDate(item.evdat)}: {item.sm_stext}
+								<strong>{formatDate(item.evdat)}</strong>: {item.sm_stext}
 							</li>
 						{/each}
 					</ul>
 				</div>
 			{/if}
 
-			{#if reminders.exams > 0}
+			{#if $drawerStore.meta.reminders.exams > 0}
 				<h3 class="h3 font-bold">Sie können sich für Prüfungsverfahren anmelden</h3>
 			{/if}
 
-			{#if reminders.electives > 0 && reminders.semester <= 6}
+			{#if $drawerStore.meta.reminders.electives > 0 && $drawerStore.meta.reminders.semester <= 6}
 				<strong>
-					<p>Anmeldung für Wahlpflichtmodule im {reminders.semester}. Fachsemester</p>
+					<p>
+						Anmeldung für Wahlpflichtmodule im {$drawerStore.meta.reminders.semester}. Fachsemester
+					</p>
 				</strong>
 			{/if}
-			<!-- </ul> -->
 		{/if}
 	</div>
 </Drawer>
