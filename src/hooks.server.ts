49492dcebf662e054ee37640930c15f33ac50699
Florian Schmidt <flo.schmidt10@gmail.com>
2024-08-05 21:33:34 +0200

    Add datenschutz/impressum + Env vars

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,29 @@
 import type { Handle } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
+import { env as publicEnv } from '$env/dynamic/public';
 
-if (!env.CD_API_URL) {
-	console.error('environment variable CD_API_URL is not set');
+if (!(env.CD_API_URL && env.MENSA_API_URL)) {
+	console.error('One or more environment variables are missing:\n\nCD_API_URL\nMENSA_API_URL\n\n');
 	process.exit(1);
 }
-if (!env.MENSA_API_URL) {
-	console.error('environment variable MENSA_API_URL is not set');
-	process.exit(1);
+
+// optional in dev mode to not waste time
+if (process.env.NODE_ENV === 'production') {
+	if (
+		!(
+			publicEnv.PUBLIC_IMPRESS_FULLNAME &&
+			publicEnv.PUBLIC_IMPRESS_STRASSE_HAUSNR &&
+			publicEnv.PUBLIC_IMPRESS_ORT_PLZ &&
+			publicEnv.PUBLIC_IMPRESS_TELEFON &&
+			publicEnv.PUBLIC_IMPRESS_EMAIL
+		)
+	) {
+		console.error(
+			'One or more environment variables are missing:\n\nPUBLIC_IMPRESS_FULLNAME\nPUBLIC_IMPRESS_STRASSE_HAUSNR\nPUBLIC_IMPRESS_ORT_PLZ\nPUBLIC_IMPRESS_TELEFON\nPUBLIC_IMPRESS_EMAIL'
+		);
+		console.warn('Optional: PUBLIC_IMPRESS_ADDRZUSATZ');
+		process.exit(1);
+	}
 }
 
 export const handle: Handle = async ({ event, resolve }) => {
